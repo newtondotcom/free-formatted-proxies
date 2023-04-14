e8a63aa306e18d0b1eb665c0924e51b453c23f6f
Robin Augereau <asphalt8fr@gmail.com>
2023-04-15 01:22:47 +0200

    oui

---
+++ main.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+
+def download(t_url):
+    response = urlopen(t_url)
+    data = response.read()
+    txt_str = str(data)
+    lines = txt_str.split("\\n")
+    des_url = 'folder/forcast.csv'
+    fx = open(des_url,"w")
+    for line in lines:
+        fx.write(line+ "\n")
+    fx.close()
+    return data
+
+#target_url = "https://spys.me/socks.txt" 
+#download(target_url)
+data = open('socks.txt', 'r').read()
+data = data.splitlines(True)
+data = data[6:]
+print(data)
+output = [ ('\"http://' + proxy.split(' ')[0] + '\"\n') for proxy in data ]
+correct = open('sources/proxy.txt', 'w').write(''.join(output))
