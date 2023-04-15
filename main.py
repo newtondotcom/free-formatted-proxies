958eb651f2c06977a368c9670599e2280b098b4a
DESKTOP-CMBB94H <asphalt8fr@gmail.com>
2023-04-16 00:10:19 +0200

    🧃

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ os.system("git config --global github.token %s" % os.getenv('GITHUB_API_TOKEN'))
 
 repo.git.add('.')
 repo.git.commit('-m', '🧃')
-origin = repo.remote(name='origin/main')
+origin = repo.remote(name='origin')
 origin.push()
 
 def update():
